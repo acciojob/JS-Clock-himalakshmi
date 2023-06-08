d15834e5d9c2176580291d3196f7ef86538df045
himalakshmi <124651043+himalakshmi@users.noreply.github.com>
2023-06-08 10:00:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+function getTime(){
+	const time = new Date();
+	let dayString = `${time.getMonth()+1}/${time.getDate()}/${time.getFullYear()}`;
+	let hr = time.getHours();
+	let timeString="";
+	if(hr>=12){
+		if(hr===12){
+			timeString=`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} PM`
+		}
+		else{
+			h-=12;
+			timeString=`${hr}:${time.getMinutes()}:${time.getSeconds()} PM`
+		}
+	}
+	else{
+		timeString=`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} AM`
+	}
+	return dayString+","+timeString;
+}
+const p = document.getElementById("timer");
+function timer() {
+	p.innerText = getTime();
+}
