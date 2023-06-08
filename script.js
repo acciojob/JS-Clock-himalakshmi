52e55f9a66ce41dcd705817773a55b6e7853cebb
himalakshmi <124651043+himalakshmi@users.noreply.github.com>
2023-06-08 10:10:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-//your code here
-function getTime(){
-	const time = new Date();
-	let dayString = `${time.getMonth()+1}/${time.getDate()}/${time.getFullYear()}`;
-	let hr = time.getHours();
-	let timeString="";
-	if(hr>=12){
-		if(hr===12){
-			timeString=`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} PM`
-		}
-		else{
-			h-=12;
-			timeString=`${hr}:${time.getMinutes()}:${time.getSeconds()} PM`
-		}
-	}
-	else{
-		timeString=`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} AM`
-	}
-	return dayString+","+timeString;
+const hourHand = document.getElementById('hour-hand');
+const minuteHand = document.getElementById('minute-hand');
+const secondHand = document.getElementById('second-hand');
+
+function rotateClockHands() {
+  // Get the current time
+  const currentTime = new Date();
+
+  // Calculate the angles for the clock hands
+  const hourAngle = (currentTime.getHours() % 12) * 30 + currentTime.getMinutes() * 0.5;
+  const minuteAngle = currentTime.getMinutes() * 6 + currentTime.getSeconds() * 0.1;
+  const secondAngle = currentTime.getSeconds() * 6;
+
+  // Apply the angles to the clock hands
+  hourHand.style.transform = `rotate(${hourAngle}deg)`;
+  minuteHand.style.transform = `rotate(${minuteAngle}deg)`;
+  secondHand.style.transform = `rotate(${secondAngle}deg)`;
 }
-const p = document.getElementById("timer");
-function timer() {
-	p.innerText = getTime();
-}
+
+// Call the rotateClockHands function every second
+setInterval(rotateClockHands, 1000);
